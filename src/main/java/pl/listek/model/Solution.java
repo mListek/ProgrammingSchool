8ab32045f63fd9931283bd297d9b7eaabdb4d0ab
Michał Listek <michal.listex@gmail.com>
2020-03-20 13:45:21 +0100

    updated solution class

--- src/main/java/pl/listek/model/Solution.java
+++ src/main/java/pl/listek/model/Solution.java
@@ -1,6 +1,69 @@
 package pl.listek.model;
 
+import java.util.Date;
+
 public class Solution {
     private int solution_id;
-    private 
+    private Date created;
+    private Date updated;
+    private String description;
+    private int exercise_id;
+    private int user_id;
+
+    public Solution() {
+    }
+
+    public Solution(Date created, Date updated, String description) {
+        this.created = created;
+        this.updated = updated;
+        this.description = description;
+    }
+
+    public int getSolution_id() {
+        return solution_id;
+    }
+
+    public void setSolution_id(int solution_id) {
+        this.solution_id = solution_id;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public Date getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(Date updated) {
+        this.updated = updated;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getExercise_id() {
+        return exercise_id;
+    }
+
+    public void setExercise_id(int exercise_id) {
+        this.exercise_id = exercise_id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
 }
