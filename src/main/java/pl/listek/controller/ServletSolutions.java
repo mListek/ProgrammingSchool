cb2e916fc7a9ca3efb580426abd369171f9e667c
Michał Listek <michal.listex@gmail.com>
2020-03-21 14:54:31 +0100

    passing recent solutions to index.jsp

--- src/main/java/pl/listek/controller/ServletSolutions.java
+++ src/main/java/pl/listek/controller/ServletSolutions.java
@@ -1,8 +1,5 @@
 package pl.listek.controller;
 
-import pl.listek.dao.SolutionDao;
-import pl.listek.model.Solution;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,17 +7,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "ServletHome")
-public class ServletHome extends HttpServlet {
+@WebServlet(name = "ServletSolutions")
+public class ServletSolutions extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        SolutionDao solutionDao = new SolutionDao();
-        int nr = Integer.parseInt(getServletContext().getInitParameter("number-solutions"));
-        Solution[] solutions = solutionDao.findRecent(nr);
-        request.setAttribute("solutions", solutions);
-        getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);;
+
     }
 }
---
+++ target/ProgrammingSchool-1.0-SNAPSHOT/header.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: mlistek
+  Date: 21.03.2020
+  Time: 13:05
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Header</title>
+</head>
+<body>
+    <p>
+        This is header
+    </p>
+</body>
+</html>
